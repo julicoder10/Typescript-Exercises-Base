79a3ef9f3d4a6fc335c4dd30f19e08d5f9bf3e02
julicoder10 <julicoder10@gmail.com>
2021-12-14 19:15:46 -0300

    My first commit on typescript exercises

---
+++ 01-tipos-basicos.ts
@@ -0,0 +1,14 @@
+/*
+    ===== Código de TypeScript =====
+*/
+
+let nombre: string = 'Julian';
+let hp: number | string = 99;
+let estaVivo: boolean = true;
+
+hp = 'FULL';
+
+
+console.log( nombre, hp );
+
+
